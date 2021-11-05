922a23f3de7751e95f2a474a80001b0c0da57d06
Max Fofanov <max.fofanov@gmail.com>
2021-11-05 10:19:14 +0300

    Initial commit

--- roguelike-spbu/Interface1.cs
+++ roguelike-spbu/Interface1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace roguelike_spbu
+﻿namespace roguelike_spbu
 {
-    interface Interface1
+    interface IEntity
     {
+
     }
 }
