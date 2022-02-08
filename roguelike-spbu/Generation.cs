b8f10131cb013fa65a314cac2ba0b1b67adde073
max-fofanov <max.fofanov@gmail.com>
2022-02-08 16:06:22 +0300

    added advanced dungeon generation with paths

--- roguelike-spbu/Generation.cs
+++ roguelike-spbu/Generation.cs
@@ -74,8 +74,69 @@ namespace roguelike_spbu {
             Random random = new Random();
 
             List<Room> rooms = new List<Room>();
-            
 
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 5; j++) {
+                    int res = Walker.Alias(new float[] { 20, 5 } );
+                    
+                    int x0 = random.Next(25 * i + 1, 25 * (i + 1) - 10);
+                    int y0 = random.Next(30 * j + 1, 30 * (j + 1) - 10);
+
+                    int x1 = 0;
+                    int y1 = 0;
+
+                    while (x1 - x0 < 10 || y1 - y0 < 10 || x1 - x0 > 20 || y1 - y0 > 20) {
+                        x1 = random.Next(x0 + 1, 25 * (i + 1));
+                        y1 = random.Next(y0 + 1, 30 * (j + 1));
+                    }
+                    
+                    rooms.Add(new Room(x0, x1, y0, y1));
+
+                    for (int a = x0; a < x1; a++) 
+                    {
+                        for (int b = y0; b < y1; b++) 
+                        {
+                            Tile tmp = new Field();
+                            tmp.X = a;
+                            tmp.Y = b;
+                            dungeon.Tiles[a][b] = tmp;
+                        }
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < rooms.Count; i++) 
+            {
+                if ((i + 1) % 5 != 0 && Math.Max(rooms[i].X0, rooms[i + 1].X0) < Math.Min(rooms[i].X1, rooms[i + 1].X1)) {
+                    
+                    int coordinate = random.Next(Math.Max(rooms[i].X0, rooms[i + 1].X0), Math.Min(rooms[i].X1, rooms[i + 1].X1));
+
+                    for (int a = rooms[i].Y1; a < rooms[i + 1].Y0 + 1; a++) {
+
+                        Tile tmp = new Field();
+                        tmp.X = coordinate;
+                        tmp.Y = a;
+                        dungeon.Tiles[coordinate][a] = tmp;
+                        
+                    }
+                }
+                if (i < 24 && Math.Max(rooms[i].Y0, rooms[i + 5].Y0) < Math.Min(rooms[i].Y1, rooms[i + 5].Y1)) {
+                    int coordinate = new Random().Next(Math.Max(rooms[i].Y0, rooms[i + 5].Y0), Math.Min(rooms[i].Y1, rooms[i + 5].Y1));
+
+                    for (int a = rooms[i].X1; a < rooms[i + 5].X0 + 1; a++) {
+
+                        Tile tmp = new Field();
+                        tmp.X = a;
+                        tmp.Y = coordinate;
+                        dungeon.Tiles[a][coordinate] = tmp;
+                        
+                    }
+                }
+            }
+
+            
+            /*
             for (int i = 0; i < 15; i++) {
 
                 bool flag = false;
@@ -149,51 +210,103 @@ namespace roguelike_spbu {
 
                 for (int a = x0; a < x1; a++) {
                     for (int b = y0; b < y1; b++) {
-                        Tile tmp = new Border();
+                        Tile tmp = new Field();
                         tmp.X = a;
                         tmp.Y = b;
                         dungeon.Tiles[a][b] = tmp;
                     }
                 }
+
             }
 
+            rooms.Sort(new RoomSorter());
+            
+            for (int i = 0; i < rooms.Count - 1; i++) 
+            {
+                if (rooms[i + 1].Y0 > rooms[i].Y0 && rooms[i + 1].Y0 < rooms[i].Y1 && rooms[i].X0 < rooms[i + 1].X0) 
+                {
+                    int coordinate = new Random().Next(rooms[i + 1].Y0, rooms[i].Y1);
+
+                    for (int a = rooms[i].X1; a < rooms[i + 1].X0 + 1; a++) {
+
+                        Tile tmp = new Field();
+                        tmp.X = a;
+                        tmp.Y = coordinate;
+                        dungeon.Tiles[a][coordinate] = tmp;
+                        
+                    }
+                }
+                else if (rooms[i + 1].Y0 > rooms[i].Y0 && rooms[i + 1].Y0 < rooms[i].Y1 && rooms[i].X0 > rooms[i + 1].X0) 
+                {
+                    int coordinate = new Random().Next(rooms[i + 1].Y0, rooms[i].Y1);
+
+                    for (int a = rooms[i + 1].X1; a < rooms[i].X0 + 1; a++) {
+
+                        Tile tmp = new Field();
+                        tmp.X = a;
+                        tmp.Y = coordinate;
+                        dungeon.Tiles[a][coordinate] = tmp;
+                        
+                    }
+                }
+
+                else if (rooms[i + 1].X0 > rooms[i].X0 && rooms[i + 1].X0 < rooms[i].X1 && rooms[i].Y0 < rooms[i + 1].Y0) 
+                {
+                    int coordinate = new Random().Next(rooms[i + 1].X0, rooms[i].X1);
+
+                    for (int a = rooms[i].Y1; a < rooms[i + 1].Y0 + 1; a++) {
+
+                        Tile tmp = new Field();
+                        tmp.X = coordinate;
+                        tmp.Y = a;
+                        dungeon.Tiles[coordinate][a] = tmp;
+                        
+                    }
+                }
+                else if (rooms[i + 1].X0 > rooms[i].X0 && rooms[i + 1].X0 < rooms[i].X1 && rooms[i].Y0 > rooms[i + 1].Y0) 
+                {
+                    int coordinate = new Random().Next(rooms[i + 1].X0, rooms[i].X1);
+
+                    for (int a = rooms[i + 1].Y1; a < rooms[i].Y0 + 1; a++) {
+
+                        Tile tmp = new Field();
+                        tmp.X = coordinate;
+                        tmp.Y = a;
+                        dungeon.Tiles[coordinate][a] = tmp;
+                        
+                    }
+                }
+
+            }
+            */
             return dungeon;
         }
 
 
     }
+
+    class RoomSorter : IComparer<Room> {
+        
+        public int Compare(Room? room1, Room? room2) {
+
+            if (room1.Y0 > room2.Y0) 
+            {
+                return 1;
+            }
+            else if (room1.Y0 < room2.Y0) 
+            {
+                return -1;
+            }
+            else if (room1.X0 < room2.X0) 
+            {
+                return 1;
+            }
+            else 
+            {
+                return -1;
+            }
+
+        }
+        
+    }
 }
-/*
-                    if (x0 >= room.X0 && y0 >= room.Y0 && x0 <= room.X1 && y0 <= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-                    else if (x1 >= room.X0 && y1 >= room.Y0 && x1 <= room.X1 && y1 <= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-                    else if (x0 >= room.X0 && y1 >= room.Y0 && x0 <= room.X1 && y1 <= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-                    else if (x1 >= room.X0 && y0 >= room.Y0 && x1 <= room.X1 && y0 <= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-                    else if (x0 <= room.X0 && y0 <= room.Y0 && x0 >= room.X1 && y0 >= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-                    else if (x1 <= room.X0 && y1 <= room.Y0 && x1 >= room.X1 && y1 >= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-                    else if (x0 <= room.X0 && y1 <= room.Y0 && x0 >= room.X1 && y1 >= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-                    else if (x1 <= room.X0 && y0 <= room.Y0 && x1 >= room.X1 && y0 >= room.Y1) {
-                        flag = true;
-                        break;
-                    }
-*/
--- roguelike-spbu/Program.cs
+++ roguelike-spbu/Program.cs
@@ -11,29 +11,11 @@ namespace roguelike_spbu
             Random rdn = new Random();
             var chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
 
-            Player player = new Player(22, 0);
-            Entity[] entities = new Entity[5];
-            Renderer renderer = new Renderer(40, 150, 10, 10);
+            Player player = new Player(0, 0);
+            Renderer renderer = new Renderer(150, 150, 10, 10);
 
-
-            for (int i = 0; i < entities.Length; i++)
-            {
-                Entity tmp = new Entity();
-                tmp.X = rdn.Next(45);
-                tmp.Y = rdn.Next(180);
-                tmp.Symbol = chars[rdn.Next(chars.Length)].ToString();
-                tmp.PrimaryForegroundColor = System.Drawing.Color.White;
-                entities[i] = tmp;
-            }
-
-            Map board1 = Generation.GenerateCave(45, 180, (22, 0), (22, 179));
-            Console.WriteLine(Renderer.Render(board1, entities, player, 0, 0));
-
-            for (int i = 0; i < entities.Length; i++)
-            {
-                Entity tmp = entities[i];
-                Console.WriteLine("{0} {1} {2}", tmp.Symbol, tmp.X, tmp.Y);
-            }
+            Map board = Generation.GenerateDungeon(150, 150);
+            Console.WriteLine(Renderer.Render(board, Array.Empty<Entity>(), player, 0, 0));
         }
     }
 }
