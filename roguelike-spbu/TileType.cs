580b63e844180b402496c5697114c1c39b592106
AgathF <pronewsky.ilya@yandex.ru>
2022-04-01 18:10:24 +0300

    tiletype

--- roguelike-spbu/TileType.cs
+++ roguelike-spbu/TileType.cs
@@ -26,16 +26,6 @@ namespace roguelike_spbu
         }
     }
     [Serializable]
-    public class Tree : Tile
-    {
-        public Tree(int x, int y) : base(x, y)
-        {
-            Symbol = "T";
-            PrimaryForegroundColor = Color.Green;
-        }
-
-    }
-    [Serializable]
     public class Rock : Tile
     {
         public Rock(int x, int y) : base(x, y)
@@ -84,6 +74,94 @@ namespace roguelike_spbu
             Symbol = "W";
             PrimaryForegroundColor = Color.Gray;
             PrimaryBackgroundColor = Color.Blue;
+        }
+
+    }
+    [Serializable]
+    public class DungeonEntry : Tile
+    {
+        public DungeonEntry(int x, int y) : base(x, y)
+        {
+            Symbol = "D";
+            PrimaryForegroundColor = Color.Red;
+            PrimaryBackgroundColor = Color.Black;
+        }
+
+    }
+    [Serializable]
+    public class House : Tile
+    {
+        public House(int x, int y) : base(x, y)
+        {
+            Symbol = "⌂";
+            PrimaryForegroundColor = Color.Gray;
+            PrimaryBackgroundColor = Color.Black;
+        }
+
+    }
+    [Serializable]
+    public class TownHall : Tile
+    {
+        public TownHall(int x, int y) : base(x, y)
+        {
+            Symbol = "♦";
+            PrimaryForegroundColor = Color.Gray;
+            PrimaryBackgroundColor = Color.Black;
+        }
+
+    }
+    [Serializable]
+    public class Shop : Tile
+    {
+        public Shop(int x, int y) : base(x, y)
+        {
+            Symbol = "☻";
+            PrimaryForegroundColor = Color.Green;
+            PrimaryBackgroundColor = Color.Black;
+        }
+
+    }
+    [Serializable]
+    public class Road : Tile
+    {
+        public Road(int x, int y) : base(x, y)
+        {
+            Symbol = "░";
+            PrimaryForegroundColor = Color.Gray;
+            PrimaryBackgroundColor = Color.Black;
+        }
+
+    }
+    [Serializable]
+    public class HubField : Tile
+    {
+        public HubField(int x, int y) : base(x, y)
+        {
+            Symbol = ".";
+            PrimaryForegroundColor = Color.Gray;
+            PrimaryBackgroundColor = Color.Black;
+        }
+
+    }
+    [Serializable]
+    public class Mountain : Tile
+    {
+        public Mountain(int x, int y) : base(x, y)
+        {
+            Symbol = "▲";
+            PrimaryForegroundColor = Color.WhiteSmoke;
+            PrimaryBackgroundColor = Color.Black;
+        }
+
+    }
+    [Serializable]
+    public class Tree : Tile
+    {
+        public Tree(int x, int y) : base(x, y)
+        {
+            Symbol = "♣";
+            PrimaryForegroundColor = Color.WhiteSmoke;
+            PrimaryBackgroundColor = Color.Black;
         }
 
     }
